6408bbe67cfa7347d9f0b76aa87718611c7ae6f1
franciscobmacedo <franciscovcbm@gmail.com>
2023-09-27 16:24:47 +0100

    add qrcode

--- run.py
+++ run.py
@@ -1,8 +1,22 @@
 import socket
 import uvicorn
 from src import settings
+import qrcode
+
+
+def main():
+    ip = socket.gethostbyname(socket.gethostname())
+    url = f"http://{ip}:{settings.PORT}"
+    qr = qrcode.QRCode()
+    qr.add_data(url)
+    print(
+        "\n\n",
+        f"Ensure your other device is on the same network and go to {url} or scan this qr code:",
+        "\n\n",
+    )
+    qr.print_ascii()
+    uvicorn.run("src.app:app", host="0.0.0.0", port=settings.PORT, reload=True)
+
 
 if __name__ == "__main__":
-    ip = socket.gethostbyname(socket.gethostname())
-    print("\n\n", f"Ensure your other device is on the same network and go to: http://{ip}:{settings.PORT}", "\n\n")
-    uvicorn.run("src.app:app", host="0.0.0.0", port=settings.PORT, reload=True)
+    main()
